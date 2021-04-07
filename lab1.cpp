bcc5ece1014ca11573547cb913701961b7c28b93
Nayana Yeshlur <nyeshlur@uw.edu>
2021-04-07 13:18:52 -0700

    working on it

--- lab1.cpp
+++ lab1.cpp
@@ -10,54 +10,55 @@ Starter code provided by Professor Dimpsey.
 #include <unistd.h>    // fork 
 #include <stdlib.h> // for exit  
 
+
 using namespace std; 
+
 
 queue<int> pids;      // stores a list of processes from the parent to a leaf process
 
 void recursive_creation(int leaves) 
 {
-
-   if (leaves > 1)
+    int leftStatus, rightStatus;
+    if (leaves > 1)
 
    { 
-       pids.push(getpid()); 
-       int leftPID = fork(); // fork a left child 
-       int rightPID = fork(); // fork a right child 
+        pids.push(getpid()); 
 
-       int leftStatus, rightStatus;
+        int leftPID = fork(); // fork a left child 
+        cout << "got here" << endl;
+        wait(&leftStatus);
+        recursive_creation(leaves - 1);
 
-       pids.push(leftPID);
-       pids.push(rightPID);
+       // int rightPID = fork(); // fork a right child
+        //wait(&rightStatus);
+        
+        /*
+        int rightPID = fork(); // fork a right child 
+        //pids.push(rightPID);
+        recursive_creation(leaves - 1);
+        wait(&rightStatus);
+        */
 
-       int result1 = pids.front();
-       pids.pop();
-       int result2 = pids.front();
-       pids.pop();
+       
 
-       cout << result1 << endl;
-       cout << result2 << endl;
-    
-       // wait for one of the children 
-       // wait for the other child
-
-       exit(0); 
+        exit(0); 
 
    } 
    else
-
    { // I'm a leaf process 
        while(pids.size() > 0)
-
        {  // print out a list of my ascendants 
+       cout << "got there" << endl;
            cout << pids.front() << " "; 
            pids.pop( ); 
        } 
+
        cout << getpid() << endl;   // print out myself 
        exit(0); 
 
    }
-
 }
+
 int main(int argc, char* argv[])
 {
 
@@ -76,7 +77,7 @@ int main(int argc, char* argv[])
       cerr << "where #leaves >= 1" << endl; 
       return -1; 
    } 
-   recursive_creation(leaves); 
+    recursive_creation(leaves); 
    return 0;
    
 }
