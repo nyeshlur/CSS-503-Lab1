4e73eb6f9208b7800e73b3233cd07c13a2baac86
Nayana Yeshlur <nyeshlur@uw.edu>
2021-04-07 12:12:47 -0700

    revised

--- lab1.cpp
+++ lab1.cpp
@@ -8,8 +8,7 @@ Starter code provided by Professor Dimpsey.
 #include <sys/types.h> // fork, wait 
 #include <sys/wait.h>  // wait 
 #include <unistd.h>    // fork 
-
-#include <stdlib.h>    // for exit  
+#include <stdlib.h> // for exit  
 
 using namespace std; 
 
@@ -22,13 +21,26 @@ void recursive_creation(int leaves)
 
    { 
        pids.push(getpid()); 
-       fork();// fork a left child 
-       // fork a right child 
+       int leftPID = fork(); // fork a left child 
+       int rightPID = fork(); // fork a right child 
+
+       int leftStatus, rightStatus;
+
+       pids.push(leftPID);
+       pids.push(rightPID);
+
+       int result1 = pids.front();
+       pids.pop();
+       int result2 = pids.front();
+       pids.pop();
+
+       cout << result1 << endl;
+       cout << result2 << endl;
     
        // wait for one of the children 
        // wait for the other child
 
-       exit (0); 
+       exit(0); 
 
    } 
    else
